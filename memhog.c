eb779d0f06952b5c69af4bce5633f08505b1591f
Gary Williams <32130780+garywill@users.noreply.github.com>
2019-02-15 09:22:41 +0000

    Update memhog.c

--- memhog.c
+++ memhog.c
@@ -4,34 +4,37 @@
 #include <string.h>
 
 int main(int argc, const char* argv[]) {
-	char* lastchar;
-	long bytes_to_hog;
-	void* mem;
-	if (argc != 2) {
-		printf("Failed: Missing argument, make sure to pass a base 10 or 16 integer (ex: 255 or 0xFF)\n");
-		return 1;
-	}
-	if (strstr(argv[1], "0x") == argv[1]) {
-		/* base 16 */
-		bytes_to_hog = strtol(argv[1], &lastchar, 16);
-	} else {
-		/* must be base 10 */
-		bytes_to_hog = strtol(argv[1], &lastchar, 10);
-	}
-	if (*lastchar != '\0') {
-		printf("Failed: Invalid argument %s, make sure to pass a base 10 or 16 integer (ex: 255 or 0xFF)\n", argv[1]);
-		return 1;
-	}
-	printf("Attempting to hog %li bytes of RAM.\n", bytes_to_hog);
- 	mem = malloc(bytes_to_hog);
-	if (!mem) {
-		printf("Failied! Couldn't malloc.\n");
-		return 1;
-	}
-	if (mlock(mem, bytes_to_hog)) {
+	long mbs;
+	unsigned char * mem;
+    long times;
+
+
+    /* must be base 10 */
+    mbs = strtol(argv[1], NULL, 10);
+    times = strtol(argv[2], NULL, 10);
+
+    for (int t = 0; t < times; ++t)
+    {
+        printf("%d Attempting to hog %li MB of RAM.\n", t, mbs);
+        mem = (unsigned char *)malloc(mbs * 1024 * 1024);
+        if (!mem) {
+            printf("Couldn't malloc.\n");
+            //return 1;
+        }
+        for (int i = 0; i < mbs * 1024 * 1024; ++i)
+        {
+            *mem = (unsigned char)rand();
+            mem++;
+        }
+        printf("Finished writing random bytes to allocated memory.\n");
+    }
+	
+	/*
+	if (mlock(mem, mbs)) {
 		printf("Failed! Couldn't mlock.\n");
-		return 1;
+		//return 1;
 	}
+	*/
 	printf("Success! Press any key to kill me and get back the RAM.\n");
 	getchar();
 	return 0;
